66989c8f16dfb0d53c2578531666e6d9700d9f69
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-11-01 18:33:00 -0300

    tipo-boolean

---
+++ tipo-boolean.js
@@ -0,0 +1,8 @@
+const estudante = 'Fernando';
+const estaAprovado = false;
+
+if(estaAprovado === true){
+    console.log(estudante + ' parabens!')
+}else{
+    console.log(estudante + ' Reprovado');
+}
