1c9c234a18ca5cf250bea23005b5cd66a190ab05
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-11-01 17:59:37 -0300

    Tipo Number e concatenação

---
+++ tipo-number.js
@@ -0,0 +1,11 @@
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBi = '5';
+
+const total = notaPrimeiroBi + notaSegundoBi + notaTerceiroBi +  notaQuartoBi;
+
+console.log(total);
+
+const nome = 'Juliana';
+console.log('Meu nome é '+ nome + notaQuartoBi);
