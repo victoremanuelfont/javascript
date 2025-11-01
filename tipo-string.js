271ce3b96839bd032f13b97e06fbaa44252c66e0
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-11-01 18:24:08 -0300

    Tipo string e funçoes prontas

---
+++ tipo-string.js
@@ -0,0 +1,8 @@
+const estudante = 'Caroline';
+const docente = 'Ana';
+const cumprimento = "Nosso lema é 'estudar bastante'";
+console.log(cumprimento);
+console.log(`A estudante se chama ${estudante}`);
+
+const senha = 'senha123' + estudante.toUpperCase();
+console.log(senha);
