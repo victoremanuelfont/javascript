be48b6dcf40000996e2874cb4b64b7271cc6f7c8
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-11-01 18:56:11 -0300

    Operadores aritméticos

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,22 @@
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = 7;
+const notaQuartoBi = 9.3;
+
+let media = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi +  notaQuartoBi)/4;
+console.log(media);
+
+if(media >= 7){
+    media += media * 0.1;
+}
+console.log(`A média é ${media}`);
+
+if(media >= 7){
+    media *= media * 0.1;
+}
+console.log(`A média é ${media}`);
+
+if(media >= 7){
+    media -= media * 0.1;
+}
+console.log(`A média é ${media}`);
