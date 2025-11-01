26a39f56cf549c21f0ce3202ca59944a93672672
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-11-01 19:02:07 -0300

    Operadores Logicos

---
+++ operadores-logicos.js
@@ -0,0 +1,15 @@
+
+const notaFinal = 6;
+const faltas = 5;
+
+if(notaFinal <7 || faltas > 4){
+    console.log('Reprovado, boas festas');
+}else{
+    console.log('Não foi reporvado por faltas');
+}
+
+if(notaFinal <7 && faltas > 4){
+    console.log('Reprovado, boas festas');
+}else{
+    console.log('Não foi reporvado por faltas');
+}
