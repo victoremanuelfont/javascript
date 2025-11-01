a1e3e0155502ab255f983bf3cd219a24018f6bad
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-11-01 18:02:09 -0300

    Transformando string em numero

--- tipo-number.js
+++ tipo-number.js
@@ -1,9 +1,9 @@
 const notaPrimeiroBi = 8;
 const notaSegundoBi = 6.3;
 const notaTerceiroBi = -2;
-const notaQuartoBi = '5';
+const notaQuartoBi = Number.parseInt('5');
 
-const total = notaPrimeiroBi + notaSegundoBi + notaTerceiroBi +  notaQuartoBi;
+const total = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi +  notaQuartoBi)/4;
 
 console.log(total);
 
